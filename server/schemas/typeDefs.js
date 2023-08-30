ee1083094e6e6c48ec83565577c4ab73ec208809
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-08-29 20:37:58 -0500

    typedef updated

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [String]!
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]!
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  
+  input savedBook {
+    bookId: String!
+    title: String!
+    image: String!
+    link: String!
+    authors: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(input: savedBook): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
